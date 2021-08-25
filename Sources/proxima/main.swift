8d5bcefb84e3fa698729bcaea9f2ed208470ccd8
Dmitrii <zeionara@gmail.com>
2021-08-25 15:46:58 +0300

    Implemented an approximate sphere volume computation using double integral

--- Sources/proxima/main.swift
+++ Sources/proxima/main.swift
@@ -1,42 +1,112 @@
 import nest
 import Foundation
 
-// public protocol Addable {
-//     static func +(_ lhs: Self, _ rhs: Self) -> Self
+// public func constantFive(_ x: Double) -> Double {
+//     return Double(5.0)
 // }
 
-// extension Double: Addable {
-//     static public func +(_ lhs: Double, _ rhs: Double) -> Double {
-//         return lhs + rhs
+// public func inverse(_ x: Double) -> Double {
+//     return 1.0 / x
+// }
+
+// let firstValue = 1.0
+// let lastValue = 3.0
+
+// let result = integrate(constantFive, from: firstValue, to: lastValue)
+
+// print("The integral of the constant function y = 5 from \(String(format: "%.3f", firstValue)) to \(String(format: "%.3f", lastValue)) is equal to \(String(format: "%.3f", result))!")
+
+// let referenceInverseFunctionResult = 1.09861228866811
+
+// for precision in stride(from: 1, to: 1001, by: 100) {
+//     let inverseFunctionResult = integrate(inverse, from: firstValue, to: lastValue, precision: precision)
+//     print(
+//         "The integral of the inverse function y = 1 / x from \(String(format: "%.3f", firstValue)) to \(String(format: "%.3f", lastValue))" + 
+//         " is equal to \(String(format: "%.5f", inverseFunctionResult))! (number of intervals = \(precision), absolute error=" +
+//         "\(String(format: "%.5f", abs(referenceInverseFunctionResult - inverseFunctionResult) / referenceInverseFunctionResult)))"
+//     )
+// }
+
+// public func add(_ lhs: Double, _ rhs: Double) -> Double {
+//     return lhs + rhs
+// }
+
+// public func computeElementarySquare(_ leftHeight: Double, _ rightHeight: Double) -> Double {
+//     let step = 0.2
+//     let lhCopy = 2.2
+//     let kind: Int = 2
+//     print(lhCopy + 0.2)
+//     if kind == 0 {
+//         return rightHeight * step
+//     } else if kind == 1 {
+//         return leftHeight * step
+//     } else {
+//         print("left height = \(leftHeight), right height = \(rightHeight)")
+//         return leftHeight + rightHeight
+//         // return add(leftHeight as! Double, rightHeight as! Double)
 //     }
 // }
 
-// public func integrate<InputType: Numeric, OutputType: Numeric>(_ getValue: (InputType) -> OutputType, from firstValue: InputType, to lastValue: InputType) -> OutputType {
-//     return getValue(firstValue) + getValue(lastValue)
+// public func integrated(
+//     _ getValue: (Double) -> Double,
+//     from firstValue: Double, to lastValue: Double, precision nIntervals: Int = 10, kind: IntegralKind = .right
+// ) -> Double {
+//     let step = abs(lastValue - firstValue) / Double(nIntervals)
+//     // print(step)
+//     return integrate(
+//         getValue, from: firstValue, to: lastValue, step: step, zero: 0.0, computeSquare: computeElementarySquare
+//     )
 // }
 
-public func constantFive(_ x: Double) -> Double {
-    return Double(5.0)
-}
-
-public func inverse(_ x: Double) -> Double {
-    return 1.0 / x
-}
-
-let firstValue = 1.0
-let lastValue = 3.0
-
-let result = integrate(constantFive, from: firstValue, to: lastValue)
-
-print("The integral of the constant function y = 5 from \(String(format: "%.3f", firstValue)) to \(String(format: "%.3f", lastValue)) is equal to \(String(format: "%.3f", result))!")
-
-let referenceInverseFunctionResult = 1.09861228866811
-
-for precision in stride(from: 1, to: 1001, by: 100) {
-    let inverseFunctionResult = integrate(inverse, from: firstValue, to: lastValue, precision: precision)
-    print(
-        "The integral of the inverse function y = 1 / x from \(String(format: "%.3f", firstValue)) to \(String(format: "%.3f", lastValue))" + 
-        " is equal to \(String(format: "%.5f", inverseFunctionResult))! (number of intervals = \(precision), absolute error=" +
-        "\(String(format: "%.5f", abs(referenceInverseFunctionResult - inverseFunctionResult) / referenceInverseFunctionResult)))"
+public func computeCircleSquare(_ r: Double, precision: Int = 10000, kind: IntegralKind = .right) -> Double {
+    return 2.0 * integrate( { (fi: Double) -> Double in
+            return r
+        },
+        from: 0,
+        to: Double.pi,
+        precision: precision,
+        kind: kind
     )
 }
+
+// print(computeCircleSquare(2.0, precision: 100, kind: .left))
+// print(computeCircleSquare(2.0, precision: 100, kind: .mean))
+// print(computeCircleSquare(2.0, precision: 100, kind: .right))
+// print(computeElementarySquare(0.2, 0.3))
+
+public func computeSphereVolume(_ r: Double, precision: Int = 10000, kind: IntegralKind = .right) -> Double {
+    return integrate( { (x: Double) -> Double in
+            let difference = r*r - x*x
+            let sqrt = difference > 0 ? difference.squareRoot() : 0
+            // print(difference, sqrt)
+            return 2.0 * computeCircleSquare(
+                sqrt,
+                precision: precision,
+                kind: kind
+            )
+        },
+        from: 0,
+        to: r,
+        precision: precision,
+        kind: kind
+    )
+}
+
+public func traceExecutionTime<Type>(_ label: String, _ function: () -> Type) -> Type {
+    let start = DispatchTime.now()
+    let result = function()
+    let end = DispatchTime.now()
+    let nSeconds = Double(end.uptimeNanoseconds - start.uptimeNanoseconds) / 1_000_000_000
+    print("\(label) has completed in \(String(format: "%.3f", nSeconds)) seconds")
+    return result
+}
+
+// print(computeSphereVolume(2.0, kind: .left))
+// print(computeSphereVolume(2.0, kind: .mean))
+// print(computeSphereVolume(2.0, kind: .right))
+
+print(
+    traceExecutionTime("sphere volume computation") {
+        computeSphereVolume(2.0, precision: 10000, kind: .right)
+    }
+)
